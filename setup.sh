6f8ad0ce2606ee0a8a66200878557850592ae730
rolecksking <uzoma.k087@gmail.com>
2026-04-21 13:21:02 +0000

    created bash script to install AWS CLI

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script sets up the environment
+echo "Downloading AWS CLI..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+echo "Unzipping..."
+unzip awscliv2.zip
+
+echo "Installing..."
+sudo ./aws/install
+
+echo "Cleaning up..."
+rm awscliv2.zip
+rm -rf aws
+
+echo "AWS CLI installed successfully!"
